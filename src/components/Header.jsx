9a7348723a22b27811a66060caf7cc71e9318ca1
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-07-18 18:16:53 -0300

    Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 
+import DevLogo from "../assets/logo.svg";
+
 export function Header() {
   return (
     <header className={`${styles.header}`}>
+      <img src={DevLogo} alt="logo devExpress"/>
       <strong>Feed devExpress</strong>
     </header>
   );
