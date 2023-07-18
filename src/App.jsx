bd4f613320c65684ee66c75cf6dfb1d7181f02ae
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-07-17 23:40:31 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Post } from "./Post";
-
+import { Header } from "./components/Header";
+import "./styles.css";
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Ronald Joaquim"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quam saepe facere eos, aspernatur perferendis? "
