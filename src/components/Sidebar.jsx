9b92a4969a46fd60c767e1347bf25c349b3c858b
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-07-19 18:26:45 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from "./Sidebar.module.css";
 
+import { PencilLine } from "phosphor-react";
+
 import backgroundImagem from "../assets/backgroundImg.jpg";
 
 export function Sidebar() {
@@ -8,12 +10,19 @@ export function Sidebar() {
       <img src={backgroundImagem} className={styles.cover} />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/ronald-Joaquim.png"
+        />
         <strong>Ronald Joaquim</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
